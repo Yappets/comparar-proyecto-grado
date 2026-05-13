1d782fbf77d15c0b5a63b6922462c2b2f9e03c94
Elias Apaza <eliasmaximilianoapaza@gmail.com>
2026-05-13 15:52:08 -0300

    Actualizar texto del filtro de productos

--- frontend/src/pages/home/HomeDesktop.tsx
+++ frontend/src/pages/home/HomeDesktop.tsx
@@ -136,8 +136,8 @@ const HomeDesktop: React.FC<Props> = ({
 
               <div className="flex items-center gap-3">
                 <span className="text-sm text-gray-700">
-                  Solo ofertas
-                </span>
+                {soloOfertas ? "Solo ofertas" : "Todos los productos"}
+              </span>
 
                 <label className="relative inline-flex items-center cursor-pointer">
                   <input
